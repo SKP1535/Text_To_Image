0aff4ce8d8bf8e93d7eb291c362acd1d685621af
Saurabh Kumar <skp1535@gmail.com>
2019-12-10 18:33:27 +0530

    Add files via upload

---
+++ text2image_exmp.py
@@ -0,0 +1,8 @@
+from texttoimage import text_2_image
+
+fontscale = 2
+filename = 'sample.png'
+thickness = 2
+textcolor = (0,0,0)
+mark = 'EXAMPLE TEXT'
+text_2_image(mark,filename,fontscale,thickness,textcolor)
